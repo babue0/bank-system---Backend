af9460406a8b20568e46bce4640bde29d5c7a3a2
babue0 <iandpcarvalho@gmail.com>
2026-02-16 21:27:04 -0300

    first commit

---
+++ src/main/java/com/br/minibanco/MiniBancoApplication.java
@@ -0,0 +1,13 @@
+package com.br.minibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBancoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MiniBancoApplication.class, args);
+  }
+
+}
