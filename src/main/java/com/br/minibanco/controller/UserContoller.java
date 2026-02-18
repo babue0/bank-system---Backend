0c5a5cd3d032d3eddc39ae3b6d6ddbe00a6fb324
babue0 <iandpcarvalho@gmail.com>
2026-02-18 20:56:14 -0300

    controlelr

---
+++ src/main/java/com/br/minibanco/controller/UserContoller.java
@@ -0,0 +1,31 @@
+package com.br.minibanco.controller;
+
+import com.br.minibanco.domain.user.User;
+import com.br.minibanco.dtos.UserDTO;
+import com.br.minibanco.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserContoller {
+
+  @Autowired
+  private UserService userService;
+
+  @PostMapping
+  public ResponseEntity<User> createUser (@RequestBody UserDTO user){
+    User newUser = userService.createUser(user);
+    return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUsers(){
+    List<User> users = userService.getAllUsers();
+    return new ResponseEntity<>(users, HttpStatus.OK);
+  }
+}
